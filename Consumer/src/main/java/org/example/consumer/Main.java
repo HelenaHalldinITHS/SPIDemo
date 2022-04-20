2dccf7cbf11fc4e0b522186674aa30d0a3c3ed39
HelenaHalldinITHS <helena.halldin@iths.se>
2022-04-20 10:36:32 +0200

    Add main-method with ServiceLoader

---
+++ Consumer/src/main/java/org/example/consumer/Main.java
@@ -0,0 +1,17 @@
+package org.example.consumer;
+
+import org.example.greet.Greeting;
+
+import java.util.ServiceLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ServiceLoader<Greeting> serviceLoader = ServiceLoader.load(Greeting.class);
+
+        for (Greeting greeting : serviceLoader) {
+            System.out.println(greeting.greet());
+        }
+    }
+
+}
