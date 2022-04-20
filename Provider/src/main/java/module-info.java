3372c025e2029561777c899f1f5eb6f36c5a4466
HelenaHalldinITHS <helena.halldin@iths.se>
2022-04-20 11:41:22 +0200

    Add info in module-info for Provider

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -1,2 +1,8 @@
-module Provider {
+import org.example.greet.Greeting;
+import org.example.provider.EnglishGreeting;
+import org.example.provider.SwedishGreeting;
+
+module org.example.greetingsprovider {
+    requires org.example.greeting;
+    provides Greeting with SwedishGreeting, EnglishGreeting;
 }
